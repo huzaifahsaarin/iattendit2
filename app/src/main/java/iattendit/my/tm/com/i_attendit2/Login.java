5bd74e0a5f88e82094670d32a4d5c05516df9085
Huzai <muhd.huzaifah@gmail.com>
2018-10-23 15:37:02 +0800

    PPM Attendance App
    PPM Attendance App for Android

---
+++ app/src/main/java/iattendit/my/tm/com/i_attendit2/Login.java
@@ -0,0 +1,23 @@
+package iattendit.my.tm.com.i_attendit2;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, First.class));
+            }
+        });
+    }
+}
