3846349a4171a0ebea451964514e6321485be45a
Huzai <muhd.huzaifah@gmail.com>
2018-10-31 10:13:50 +0800

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/iattendit/my/tm/com/i_attendit2/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package iattendit.my.tm.com.i_attendit2;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "iattendit.my.tm.com.i_attendit2";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
